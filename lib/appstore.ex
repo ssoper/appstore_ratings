dc6440ee7f97fa328f454533180f54f62a6c3b88
Sean Soper <sean.soper@gmail.com>
2014-09-29 16:10:10 -0400

    set user-agent header

--- lib/appstore.ex
+++ lib/appstore.ex
@@ -4,4 +4,9 @@ defmodule Appstore do
   def process_url(app_id) do
     "https://itunes.apple.com/us/app/" <> app_id
   end
+
+  def process_request_headers(headers) do
+    [:user_agent, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36"]
+  end
+
 end
